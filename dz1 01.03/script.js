7fc04f75ac9e745a19dc658878fd5f2aa05161a9
dastan <dsoltobaevu@gmail.co>
2022-03-12 16:02:23 +0600

    feat: add refact

---
+++ dz1 01.03/script.js
@@ -0,0 +1,38 @@
+// 1) Создайте объект заработных плат.
+//     Назовите userSalaries.
+//     Свойствами выступают: Ella, Sophia, Ellie
+// Выведите на экран зарплату Ella, Sophia.
+//
+
+                // var obj = {'Ella':'17000', 'Sophia':'15000', 'Ellie':'20000'};
+                // alert(obj['Ella']+ '\r' + obj['Sophia']);
+
+
+
+// 2) Создайте объект с ключами a, b и c значениями 5 , 6 и 10.
+// Найдите сумму его элементов.
+
+                // var obj = {a: 5, b: 6, c: 10};
+                // alert(obj[a+b+c]);
+
+
+//
+//
+
+
+
+                let number;
+
+                // if(num === true){
+                //     number = 3;
+                // } else{
+                //     number = 5;
+                // }
+
+                let result = number === true ? number=5 : number=3;
+                console.log(result)
+
+
+
+//
+// Сделайте рефакторинг кода, с помощью тернарного оператора.
